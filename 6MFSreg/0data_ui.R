5e02d0505feb301fd00a1bc1f003a1c0d45e1f60
yi-apr <zhouyi0426@hotmail.com>
2018-12-20 13:14:20 +0900

    Update reg

---
+++ 6MFSreg/0data_ui.R
@@ -0,0 +1,120 @@
+##----------------------------------------------------------------
+##
+## The regression models: lm, logistic model, cox model, ui
+## 
+##    0. data preparation
+## 
+## DT: 2018-12-13
+##
+##----------------------------------------------------------------
+
+
+#tabPanel("Dataset",
+
+  #titlePanel("Upload your dataset"),
+
+#helpText("Before user's data is uploaded, the example data (mtcars) is being shown ."),
+  sidebarLayout(
+
+    sidebarPanel(
+
+    ##----------example datasets----------
+
+    selectInput("edata", "Choose data:", 
+                            choices =  c("insurance","advertisement","lung"), 
+                            selected = "insurance"),
+                ## render dynamic checkboxes
+    
+
+
+    ##-------csv file-------##   
+    fileInput('file', "Upload .csv",
+              accept = c("text/csv",
+                      "text/comma-separated-values,text/plain",
+                      ".csv")),
+    helpText("The columns of X are not suggested greater than 500"),
+    # Input: Checkbox if file has header ----
+    checkboxInput("header", "Header", TRUE),
+
+      fluidRow(
+
+      column(4, 
+         # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ',',
+                               Semicolon = ';',
+                               Tab = '\t'),
+                   selected = ',')),
+
+      column(4,
+      # Input: Select quotes ----
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'))
+      ),
+
+      actionButton("choice", "Import dataset", style="color: #fff; background-color: #337ab7; border-color: #2e6da4")),
+
+
+    mainPanel(
+      
+      h4(tags$b("Data Display")),
+
+      tags$br(),
+
+      tags$b("Overview of the first 5 row and 2 columns of the dataset"), 
+
+      dataTableOutput("data"),
+  
+      
+      selectInput("columns", "Select variables to display the details", choices = NULL, multiple = TRUE), # no choices before uploading 
+  
+      dataTableOutput("data_var"),
+      hr(),
+
+      h4(tags$b("Basic Descriptives")), 
+      tags$b("Select the variables for descriptives"),
+
+        fluidRow(
+          column(6,
+          uiOutput('cv'),
+          actionButton("Bc", "Show descriptives"),
+          tableOutput("sum"),
+          helpText(HTML(
+      "
+      Note:
+      <ul>
+      <li> nbr.: the number of </li>
+      </ul>
+      "
+      ))
+          ),
+
+          column(6,
+          uiOutput('dv'),
+          actionButton("Bd", "Show descriptives"),
+          verbatimTextOutput("fsum")
+          )),
+
+      h4(tags$b("First Exploration of Variables")),  
+      tabsetPanel(
+        tabPanel("Scatter plot (with line) between two variables",
+          uiOutput('ty'),
+          uiOutput('tx'),
+          plotOutput("p1", width = "400px", height = "400px")
+          ),
+        tabPanel("Bar plots",
+          fluidRow(
+          column(6,
+            uiOutput('hx'),
+            plotOutput("p2", width = "400px", height = "400px"),
+            sliderInput("bin", "The width of bins in the histogram", min = 0.01, max = 50, value = 1)),
+          column(6,
+            uiOutput('hxd'),
+            plotOutput("p3", width = "400px", height = "400px"))))
+        )
+   
+
+  ))
---
+++ 6MFSreg/1lm_ui.R
@@ -0,0 +1,128 @@
+##----------------------------------------------------------------
+##
+## The regression models: lm, logistic model, cox model, ui
+## 
+##    1. linear regression
+## 
+## DT: 2018-12-13
+##
+##----------------------------------------------------------------
+
+#tabPanel("Linear Regression (Continuous Outcomes)",
+
+#titlePanel("Linear Regression"),
+
+sidebarLayout(
+
+  sidebarPanel(
+
+  h4(tags$b("Given that dataset has been imported, please design you model")),      
+  uiOutput('y'),    
+  uiOutput('x'),
+  uiOutput('fx'),
+  
+  radioButtons("intercept", "Intercept", ##> intercept or not
+               choices = c("Remove Intercept" = "-1",
+                           "Keep intercept" = ""),
+               selected = "-1"),
+  h5("Additional terms (confounding or interaction)"), 
+  helpText('Note: Start with "+". For interactive term, please type "+ as.factor(var1):var2"'), 
+  tags$textarea(id='conf', cols=40, " " ), 
+  p(br()),
+  actionButton("F", "Create formula", style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+
+  ),
+ 
+mainPanel(
+
+  h4(tags$b("Linear Regression Model")),
+  
+  tags$style(type='text/css', '#formula {background-color: rgba(0,0,255,0.10); color: blue;}'),
+  verbatimTextOutput("formula", placeholder = TRUE),
+  helpText("Note: '-1' in the formula indicates that intercept has been removed"),
+  hr(),
+
+
+  h4(tags$b("Results of the linear regression")),
+  actionButton("B1", "Show the results"),
+  p(br()),
+  tabsetPanel(
+    
+    tabPanel("Parameters' estimation",
+      p(br()),
+     #sliderInput("range", label = h3("choose subset"), min = 1, max = 100, value = c(1,10)),
+      tags$b("1. Regression's coefficients"),
+      htmlOutput("fit"), p(br()),
+      tags$b("2. ANOVA Table"), tableOutput("anova"),p(br()),
+      tags$b("3. Select a formula-based model by AIC"), verbatimTextOutput("step")
+      ),
+
+    tabPanel("Model's diagnostics", 
+      p(br()),
+      tags$b("Diagnostic Plots"), 
+      radioButtons("num", "Choose plot",
+                   choices = c("Residuals vs fitted plot" = 1,
+                               "Normal Q-Q" = 2,
+                               "Scale-Location" = 3,
+                               "Cook's distance" = 4,
+                               "Residuals vs Leverage" = 5),
+                   selected = 1),
+      plotOutput("p.lm", width = "800px", height = "400px")
+      ),
+
+    tabPanel("Estimated fitting values",
+      p(br()),
+      tags$b("Estimation is based on import dataset"),
+      dataTableOutput("fitdt0")),
+
+      tabPanel("Prediction on new data", p(br()),
+      #prediction part
+        ##-------csv file for prediction -------##   
+      # Input: Select a file ----
+      fileInput("newfile", "Upload new .csv data set",
+                multiple = TRUE,
+                accept = c("text/csv",
+                         "text/comma-separated-values,text/plain",
+                         ".csv")),
+       # Input: Checkbox if file has header ----
+      checkboxInput("newheader", "Header", TRUE),
+
+      fluidRow(
+      column(3, 
+         # Input: Select separator ----
+      radioButtons("newsep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ",")),
+
+      column(3,
+        # Input: Select quotes ----
+      radioButtons("newquote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"')),
+      column(3,
+       # prediction type
+      radioButtons("interval", "Choose predictive interval (0.95-level)",
+                   choices = c(
+                               "Confidence Interval" = "confidence",
+                               "Prediction Interval" = "prediction"),
+                   selected = 'confidence'))
+                ), ##fluidRow(
+
+      actionButton("B2", "Submit after the estimation of model"), 
+      helpText("If no data is uploaded, the example testing data (the first 10 rows of import dataset) will be shown."),
+      
+      p(br()),
+      tags$b("Data display with prediction results"), 
+      p(br()),
+      dataTableOutput("pred")
+      
+  ) 
+
+
+  )
+  )
+)
