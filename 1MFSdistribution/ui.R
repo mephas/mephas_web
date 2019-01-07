fa593109d559e16964cb64e5bc803bcd43ec1183
yi-apr <zhouyi0426@hotmail.com>
2019-01-07 17:28:05 +0900

    push first

--- 1MFSdistribution/ui.R
+++ 1MFSdistribution/ui.R
@@ -416,7 +416,6 @@ tabPanel(
      id = 'close',
      type = "button",
      class = "btn action-button",
-     onclick ="window.open('https://pharmacometrics.info/mephas/index_jp.html')","top"))
 
 
 ))
