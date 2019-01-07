6a9bb3f2a7fe762e6e4eb3fd79bc41c9adb47f52
yi-apr <zhouyi0426@hotmail.com>
2019-01-07 17:28:34 +0900

    push first2

--- 2MFSttest/ui.R
+++ 2MFSttest/ui.R
@@ -447,6 +447,7 @@ tabPanel(
      id = 'close',
      type = "button",
      class = "btn action-button",
+     onclick ="window.open('https://pharmacometrics.info/mephas/index_jp.html')","Main Page"))
 
   )
 )
