77e2e55729c6cd8a61b5e6c7377b95c638ad7685
Yi <yi@Yi-pro.local>
2019-03-21 17:30:00 +0900

    help1

--- 1MFSdistribution/ui.R
+++ 1MFSdistribution/ui.R
@@ -415,7 +415,8 @@ tableOutput("p.k")
 ##---------- other panels ----------
 
 source("../0tabs/home.R",local=TRUE, encoding="UTF-8")$value,
-source("../0tabs/stop.R",local=TRUE, encoding="UTF-8")$value
+source("../0tabs/stop.R",local=TRUE, encoding="UTF-8")$value,
+source("../0tabs/help1.R",local=TRUE, encoding="UTF-8")$value
 
 
 ))
