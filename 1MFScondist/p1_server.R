4a1318549b70e9157d706b4030ea59e3d894f49c
Yi <yi@Yi-pro.local>
2019-12-18 12:50:16 +0900

    push back

--- 1MFScondist/p1_server.R
+++ 1MFScondist/p1_server.R
@@ -110,20 +110,27 @@ output$sum2 = renderTable({
   }, digits = 4, colnames=FALSE, rownames=TRUE, width = "500px")
 
 
-output$downloadReport <- downloadHandler(
-    filename = function() {
-      paste('my-report', sep = '.', switch(
-        input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
-      ))
-    },
+      output$report <- downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.html",
+        content = function(file) {
+          # Copy the report file to a temporary directory before processing it, in
+          # case we don't have write permissions to the current working dir (which
+          # can happen when deployed).
+          tempReport <- file.path(tempdir(), "report.Rmd")
+          file.copy("report.Rmd", tempReport, overwrite = TRUE)
+          
+          # Set up parameters to pass to Rmd document
+          params <- list(n = input$bin1)
+          
+          # Knit the document, passing in the `params` list, and eval it in a
+          # child of the global environment (this isolates the code in the document
+          # from the code in this app).
+          rmarkdown::render(tempReport, output_file = file,
+                            params = params,
+                            envir = new.env(parent = globalenv())
+          )
+        }
+      )
 
-    content = function(file) {
-
-      tempReport <- file.path(getwd(), "report.rmd")
-      file.copy("report.rmd", tempReport, overwrite = TRUE)
-
-      
-      rmarkdown::render(tempReport, output_file = file,
-                        envir = new.env(parent = globalenv()))
- })
 
