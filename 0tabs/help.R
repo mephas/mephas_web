324554cffa4dafb1945b96d0d178592956d79978
Yi <yi@Yi-pro.local>
2020-01-16 15:27:51 +0900

    Update help.R

--- 0tabs/help.R
+++ 0tabs/help.R
@@ -1,4 +1,4 @@
 tabPanel((a("Tutorial and Help",
             target = "_blank",
             style = "margin-top:-30px; color:DodgerBlue",
-            href = paste0("https://alain003.phs.osaka-u.ac.jp/mephas/","help1.html"))))
+            href = paste0("https://mephas.github.io/helppage/"))))
