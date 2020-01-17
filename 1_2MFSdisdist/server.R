f9b96d7be423dcdae6a91af808e37b2e93b26734
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2020-01-17 11:52:30 +0900

    update

--- 1_2MFSdisdist/server.R
+++ 1_2MFSdisdist/server.R
@@ -1,17 +1,5 @@
 if (!require(shiny)) {install.packages("shiny")}; library(shiny)
 if (!require(ggplot2)) {install.packages("ggplot2")}; library(ggplot2)
-#if (!require(plotly)) {install.packages("plotly")}; library(plotly)
-#if (!require(DT)) {install.packages("DT")}; library(DT)
-##----------#----------#----------#----------
-##
-## 1MFSdistribution SERVER
-##
-## Language: EN
-## 
-## DT: 2019-01-08
-## Update: 2019-12-05
-##
-##----------#----------#----------#----------
 
 shinyServer(
 
