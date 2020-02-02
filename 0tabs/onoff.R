296e24d786df1370fbabeec6d3cf1d4208fb2443
Yi <yi@Yi-pro.local>
2020-02-02 21:01:49 +0900

    Update onoff.R

--- 0tabs/onoff.R
+++ 0tabs/onoff.R
@@ -23,7 +23,7 @@
 
 fluidPage(
 
-	switchInput(#
+	shinyWidgets::switchInput(#
 			   inputId = "explain_on_off",#
 			   label = "<i class=\"fa fa-book\"></i>", # Explanation in Details
 			    inline = TRUE,
