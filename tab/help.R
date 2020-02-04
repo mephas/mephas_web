be49c1b254e336647e794df6c4177fb81b1462e6
Yi <yi@Yi-pro.local>
2020-02-04 09:51:48 +0900

    remove files

--- tab/help.R
+++
@@ -1,9 +0,0 @@
-tabPanel(
-		a(#"Help",#
-    target = "_blank",
-    #style = "margin-top:-30px; color:DodgerBlue",
-    href = paste0("https://mephas.github.io/helppage/")),#,
-		icon = icon("question")
-)
-
-#tabPanel("",icon = icon("question"),value = "help")
