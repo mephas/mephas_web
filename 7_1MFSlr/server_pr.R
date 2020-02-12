274e68c27fe98bfc4a564191d98f54044fb38537
Yi <yi@Yi-pro.local>
2020-02-12 16:08:46 +0900

    Update server_pr.R

--- 7_1MFSlr/server_pr.R
+++ 7_1MFSlr/server_pr.R
@@ -7,13 +7,14 @@ newX = reactive({
     }
   else{
 if(!input$newcol){
-    csv <- read.csv(inFile$datapath, header = input$newheader, sep = input$newsep, quote=input$newquote)
+    csv <- read.csv(inFile$datapath, header = input$newheader, sep = input$newsep, quote=input$newquote,stringsAsFactors=TRUE)
     }
     else{
-    csv <- read.csv(inFile$datapath, header = input$newheader, sep = input$newsep, quote=input$newquote, row.names=1)
+    csv <- read.csv(inFile$datapath, header = input$newheader, sep = input$newsep, quote=input$newquote, row.names=1,stringsAsFactors=TRUE)
     }
     validate( need(ncol(csv)>1, "Please check your data (nrow>1, ncol>1), valid row names, column names, and spectators") )
     validate( need(nrow(csv)>1, "Please check your data (nrow>1, ncol>1), valid row names, column names, and spectators") )
+    validate(need(match(input$x, colnames(csv)), "New data do not cover all the independent variables"))
 
   x <- as.data.frame(csv)
 }
@@ -22,11 +23,12 @@ return(as.data.frame(x))
 #prediction plot
 # prediction
 pred = eventReactive(input$B2,
-{predict(fit(), newdata = newX())
+{
+  predict(fit(), newdata = newX())
 })
 
 pred.lm <- reactive({
-	round(cbind.data.frame("Predicted Y"=(pred()),newX()),6)
+	cbind.data.frame("Predicted Y"=round(pred(),6),newX())
 	})
 
 output$pred = DT::renderDT({
@@ -39,7 +41,7 @@ buttons = c('copy', 'csv', 'excel'),
 scrollX = TRUE))
 
  output$p.s = plotly::renderPlotly({
-  validate(need((pred.lm()[, input$y]), "This evaluation plot will not show unless dependent variable Y is given in the new data"))
+  validate(need(input$y %in% names(newX()), "This evaluation plot will not show unless dependent variable Y is given in the new data"))
   #min = min(c(pred.lm()[, input$y], pred.lm()[, 1]))
   #max = max(c(pred.lm()[, input$y], pred.lm()[, 1]))
   x <- pred.lm()
