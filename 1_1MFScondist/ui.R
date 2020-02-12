c43a78e457678260138e4c6d315df48cbb10ba24
Yi <yi@Yi-pro.local>
2020-02-12 14:48:50 +0900

    Update ui.R

--- 1_1MFScondist/ui.R
+++ 1_1MFScondist/ui.R
@@ -2,7 +2,7 @@
 
 if (!require("shiny")) {install.packages("shiny")}; library("shiny")
 if (!require("ggplot2")) {install.packages("ggplot2")}; library("ggplot2")
-#if (!require("plotly")) {install.packages("plotly")}; library("plotly")
+if (!require("plotly")) {install.packages("plotly")}; library("plotly")
 #if (!require("shinyWidgets")) {install.packages("shinyWidgets")}; library("shinyWidgets")
 
 
