a62939c18997ec69b815feb49b39a4a011237bee
amenokoyushi <amenokoyushi@gmail.com>
2021-02-09 17:51:16 +0900

    3MF途中
    3MF途中

---
+++ 3MFSnptest_cn/ui_1.R
@@ -0,0 +1,149 @@
+#****************************************************************************************************************************************************1.np1
+
+sidebarLayout(
+
+sidebarPanel(
+
+  h4(tags$b("第1步  准备数据")),
+
+  p(tags$b("1. 命名数据（必填）")),
+
+  tags$textarea(id="cn", rows= 1, "Scale"), p(br()),
+
+  p(tags$b("2. 数据输入")),
+
+  tabsetPanel(
+  ##-------input data-------##
+  tabPanel("手动输入", p(br()),
+    conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("这里的数据是一组9名患者的抑郁评定量表(DRS) 测量结果. "))
+    ),
+
+    p(tags$b("请参考示例格式上传数据")),
+    p("数据点可以用 逗号（,） 分号（;） 回车（/Enter） 制表符（/Tab） 空格（/Space）分隔"),
+    p(tags$b("从CSV（一列）复制数据并粘贴到框中")),   
+    tags$textarea(id="a",
+      rows=5,
+      "1.83\n0.50\n1.62\n2.48\n1.68\n1.88\n1.55\n3.06\n1.30"
+      ),
+
+    p("缺失值输入NA")
+
+    ),
+  tabPanel.upload.num(file ="file", header="header", col="col", sep="sep")
+
+    ),
+
+  hr(),
+  h4(tags$b("第2步  指定参数")),
+  numericInput("med", HTML("指定要与数据进行比较的中位数(m&#8320)"), 1),
+  conditionalPanel(
+    condition = "input.explain_on_off",
+  p(tags$i("在初始设定里，我们想要知道这组患者是否被抑郁症所困扰(测量结果 > 1)。"))
+    ),
+  hr(),
+
+  h4(tags$b("第3步  选择假设")),
+  p(tags$b("虚假设")),
+
+  HTML("<p> m = m&#8320: 总体中位数等于指定中位数( m&#8320) </p>
+        <p>或，数据集的分布关于指定中位数对称</p>"),
+
+  radioButtons("alt.wsr",
+    label = "备择假设", selected = "greater",
+    choiceNames = list(
+    HTML("m &#8800 m&#8320: 总体中位数与指定中位数有显著差异"),
+    HTML("m > m&#8320: 总体中位数大于指定中位数"),
+    HTML("m < m&#8320: 总体中位数小于指定中位数")
+    ),
+  choiceValues = list("two.sided", "greater", "less")),
+  hr(),
+  conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("在初始设定里，我们想要知道这组患者是否被抑郁症所困扰(测量结果 > 1)。"))
+    ),
+
+  h4(tags$b("第4步  测定P值的方法")),
+  radioButtons("alt.md",
+    label = "数据是什么样的", selected = "c",
+    choiceNames = list(
+      HTML("近似正态分布P值：样本量大"),
+      HTML("渐近正态分布P值：样本量大"),
+      HTML("精确P值：样本量小 (<50)")
+      ),
+    choiceValues = list("a", "b", "c")),
+      conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("在这个示例里，我们只有9名患者。因此，我们使用精确P值。"))
+    )
+
+  ),
+
+mainPanel(
+
+  h4(tags$b("Output 1. 描述性统计结果")),
+
+  tabsetPanel(
+
+    tabPanel("数据确认", p(br()),
+      DT::DTOutput("table")
+      ),
+
+    tabPanel("描述性统计量", p(br()),
+
+        DT::DTOutput("bas")
+      ),
+
+    tabPanel("箱形图", p(br()),
+
+        plotly::plotlyOutput("bp"), #, click = "plot_click"
+
+        #verbatimTextOutput("info"), hr(),
+
+          HTML(
+          "<b> 说明 </b>
+          <ul>
+            <li> 框内的条带为中位数</li>
+            <li> 方框测量了第75和第25个百分位数之间的差值</li>
+            <li> 如果存在离群值，将显示为红色</li>
+          </ul>"
+
+          )
+      ),
+
+    tabPanel("直方图", p(br()),
+            HTML(
+          "<b> 说明 </b>
+          <ul>
+            <li> 直方图：通过描述某一数值范围内出现的观察值频率，粗略评估给定变量的概率分布</li>
+            <li> 密度图：估计数据的概率密度函数</li>
+          </ul>"
+            ),
+      p(tags$b("直方图")),
+      plotly::plotlyOutput("makeplot"),
+      sliderInput("bin","直方图的分箱数",min = 0,max = 100,value = 0),
+      p("当分箱数为0时，绘图将使用默认分箱数"),
+      p(tags$b("密度图")),
+      plotly::plotlyOutput("makeplot.1")
+
+      )
+    ),
+hr(),
+h4(tags$b("Output 2. 检验结果")),
+    p(tags$b('威尔科克森符号秩检验结果')), p(br()),
+    DT::DTOutput("ws.test.t"),
+
+    HTML(
+    "<b> 说明 </b>
+    <ul>
+    <li> P值 < 0.05，则总体中位数与指定中位数有显著差异。（接受备择假设）</li>
+    <li> P值 >= 0.05，则总体中位数与指定中位数无显著差异。（接受虚假设）</li>
+    </ul>"
+  ),
+    conditionalPanel(
+    condition = "input.explain_on_off",
+  p(tags$i("根据初始设置, 我们可以得到测量结果显著大于1的结论 (P = 0.006)，表明患者处于抑郁症的困扰."))
+  )#,
+  )
+)
---
+++ 3MFSnptest_cn/ui_p.R
@@ -0,0 +1,153 @@
+#****************************************************************************************************************************************************3.npp
+
+sidebarLayout(
+
+sidebarPanel(
+
+  h4(tags$b("Step 1. Data Preparation")),
+  p(tags$b("1. Give names to your data (Required)")),
+
+  tags$textarea(id="cn3", rows=3, "Before\nAfter\nAfter-Before"), p(br()),
+
+
+  p(tags$b("2. Input data")),
+
+  tabsetPanel(
+
+  tabPanel("Manual Input", p(br()),
+        conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("The example shown here was the Depression Rating Scale factor measurements of 9 patients Before and After treatment. "))
+      ),
+
+    p(tags$b("Please follow the example to input your data")),
+    p("Data point can be separated by , ; /Enter /Tab /Space"),
+    p(tags$b("Data be copied from CSV (one column) and pasted in the box")), 
+      
+    p(tags$b("Before")),
+    tags$textarea(id="y1",
+      rows=10,
+    "1.83\n0.50\n1.62\n2.48\n1.68\n1.88\n1.55\n3.06\n1.30"
+    ),
+
+    p(tags$b("After")),
+    tags$textarea(id="y2",
+      rows=10,
+      "0.88\n0.65\n0.59\n2.05\n1.06\n1.29\n1.06\n3.14\n1.29"
+      ),
+
+    p("Missing values are input as NAs to ensure 2 sets have equal length; otherwise, there will be error")
+
+),
+
+tabPanel.upload.num(file ="file3", header="header3", col="col3", sep="sep3")
+
+    ),
+
+  hr(),
+
+  h4(tags$b("Step 2. Choose Hypothesis")),
+
+  p(tags$b("Null hypothesis")),
+  HTML("<p>  m = 0: the difference of medians between X and Y is zero </p>
+        <p>  Or, the distribution of the differences in paired values is symmetric around zero</p> "),
+
+  radioButtons("alt.wsr3", label = "Alternative hypothesis",
+    choiceNames = list(
+      HTML("m &#8800 0: the difference of medians between X and Y is not zero; the distribution of the differences in paired values is not symmetric around zero"),
+      HTML("m < 0: the population median of Y is greater"),
+      HTML("m > 0: the population median of X is greater")),
+    choiceValues = list("two.sided", "less", "greater")),
+      conditionalPanel(
+    condition = "input.explain_on_off",
+      p(tags$i("In this example, we wanted to know if there was a significant difference on the scale after the treatment. "))
+        ),
+hr(),
+
+h4(tags$b("Step 3. Decide P Value method")),
+radioButtons("alt.md3",
+    label = "What is the data like", selected = "c",
+    choiceNames = list(
+      HTML("Approximate normal distributed P value: sample size is large"),
+      HTML("Asymptotic normal distributed P value: sample size is large"),
+      HTML("Exact P value: sample size is small (< 50)")
+      ),
+    choiceValues = list("a", "b", "c")),
+    conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("In this example, we had only 9 people. So we chose the exact P value"))
+    )
+
+  ),
+
+mainPanel(
+
+  h4(tags$b("Output 1. Descriptive Results")),
+
+  tabsetPanel(
+
+    tabPanel("Data Preview", p(br()),
+
+      DT::DTOutput("table3")
+      ),
+
+    tabPanel("Basic Descriptives", p(br()),
+
+        DT::DTOutput("bas3")
+  ),
+
+    tabPanel("Box-Plot of the Difference", p(br()),
+
+        plotly::plotlyOutput("bp3"),#click = "plot_click3"
+
+        verbatimTextOutput("info3"), hr(),
+
+          HTML(
+          "Notes:
+          <ul>
+            <li> The band inside the box is the median</li>
+            <li> The box measures the difference between 75th and 25th percentiles</li>
+            <li> Outliers will be in red, if existing</li>
+          </ul>"
+
+          )
+      ),
+
+    tabPanel("Histogram", p(br()),
+            HTML(
+          "Notes:
+          <ul>
+            <li> Histogram: to roughly assess the probability distribution of a given variable by depicting the frequencies of observations occurring in certain ranges of values</li>
+            <li> Density Plot: to estimate the probability density function of the data</li>
+          </ul>"
+            ),
+      p(tags$b("Histogram")),
+      plotly::plotlyOutput("makeplot3"),
+      sliderInput("bin3", "The number of bins in histogram",min = 0,max = 100,value = 0),
+      p("When the number of bins is 0, plot will use the default number of bins"),
+      p(tags$b("Density plot")),
+      plotly::plotlyOutput("makeplot3.1")
+
+      )
+    ),
+
+    hr(),
+
+  h4(tags$b("Output 2. Test Results")),p(br()),
+  tags$b('Results of Wilcoxon Signed-Rank Test'),
+    DT::DTOutput("psr.test.t"),
+      HTML(
+    "<b> Explanations </b>
+    <ul>
+    <li> P Value < 0.05, then the Before and After are significantly different. (Accept the alternative hypothesis)</li>
+    <li> P Value >= 0.05, then the Before and After are NOT significantly different. (Accept the null hypothesis)</li>
+    </ul>"
+  ),
+      conditionalPanel(
+    condition = "input.explain_on_off",
+    p(tags$i("From the default settings, we concluded that there was no significant difference after the treatment. (P=0.46)"))
+    )
+
+
+)
+)
