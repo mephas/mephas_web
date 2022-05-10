3eb92eb295ce60db8772b64652502942917a3bb2
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-05-10 20:00:50 +0900

    a

--- 11DTA-Meta/ui.R
+++ 11DTA-Meta/ui.R
@@ -1,12 +1,12 @@
 
 if (!requireNamespace("shiny", quietly = TRUE)) {install.packages("shiny")}; require("shiny",quietly = TRUE)
 if (!requireNamespace("ggplot2",quietly = TRUE)) {install.packages("ggplot2")}; require("ggplot2",quietly = TRUE)
-if (!requireNamespace("shinyWidgets", quietly = TRUE)) {install.packages("shinyWidgets")}; require("shinyWidgets",quietly = TRUE)
-if (!requireNamespace("latex2exp", quietly = TRUE)) {install.packages("latex2exp")}; require("latex2exp",quietly = TRUE)
-if (!requireNamespace("shinythemes", quietly = TRUE)) {install.packages("shinythemes")}; require("shinythemes",quietly = TRUE)
-if (!requireNamespace("DT", quietly = TRUE)) {install.packages("DT")}; require("DT",quietly = TRUE)
-if (!requireNamespace("meta", quietly = TRUE)) {install.packages("meta")}; require("meta",quietly = TRUE)
-if (!requireNamespace("mada", quietly = TRUE)) {install.packages("mada")}; require("mada",quietly = TRUE)
+# if (!requireNamespace("shinyWidgets", quietly = TRUE)) {install.packages("shinyWidgets")}; require("shinyWidgets",quietly = TRUE)
+# if (!requireNamespace("latex2exp", quietly = TRUE)) {install.packages("latex2exp")}; require("latex2exp",quietly = TRUE)
+# if (!requireNamespace("shinythemes", quietly = TRUE)) {install.packages("shinythemes")}; require("shinythemes",quietly = TRUE)
+# if (!requireNamespace("DT", quietly = TRUE)) {install.packages("DT")}; require("DT",quietly = TRUE)
+# if (!requireNamespace("meta", quietly = TRUE)) {install.packages("meta")}; require("meta",quietly = TRUE)
+# if (!requireNamespace("mada", quietly = TRUE)) {install.packages("mada")}; require("mada",quietly = TRUE)
 
 source("function.R")
 #source("./ui_dtameta.R",local = TRUE)
