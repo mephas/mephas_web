9eb9eb39198cb65522513ec65ef2a8179c5dfeae
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-05-11 13:17:57 +0900

    a

--- 11DTA-Meta/ui.R
+++ 11DTA-Meta/ui.R
@@ -7,6 +7,7 @@ if (!requireNamespace("ggplot2",quietly = TRUE)) {install.packages("ggplot2")}; 
 if (!requireNamespace("DT", quietly = TRUE)) {install.packages("DT")}; require("DT",quietly = TRUE)
 if (!requireNamespace("meta", quietly = TRUE)) {install.packages("meta")}; require("meta",quietly = TRUE)
 if (!requireNamespace("mada", quietly = TRUE)) {install.packages("mada")}; require("mada",quietly = TRUE)
+if (!requireNamespace("dtametasa", quietly = TRUE)) {install.packages("dtametasa")}; require("dtametasa",quietly = TRUE)
 
 source("function.R")
 #source("./ui_dtameta.R",local = TRUE)
