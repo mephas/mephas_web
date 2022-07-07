260d8340a74d875eb7cd371045894c12aa0ee04a
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-07-07 21:05:47 +0900

    server

--- 11DTA-Meta/server.R
+++ 11DTA-Meta/server.R
@@ -3,8 +3,8 @@
 function(input, output, session) {
   source("./server_outline.R",local = TRUE)
   source("./server_uni.R",local = TRUE)
-  source("./server_dta.R",local = TRUE)
-  
+  #source("./server_dta.R",local = TRUE)
+  source("./server_dtameta.R",local = TRUE)
   observe({
     if (input$close > 0) stopApp()                             # stop shiny
   })
