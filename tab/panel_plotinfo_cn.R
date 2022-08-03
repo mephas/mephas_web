130211de7ca056b838e60cefaaf37b204c05124c
yz-apr <zhouyi0426@hotmail.com>
2022-08-03 15:23:33 +0900

    compact nptest

--- tab/panel_plotinfo_cn.R
+++ tab/panel_plotinfo_cn.R
@@ -6,7 +6,7 @@
 ## Add new panel for T test
 
 ##
-tabPanel.boxplot <- function(plot.id, width ="80%"){
+tabPanel.boxplot <- function(plot.id, width =500){
 
 tabPanel("箱型图", 
 
@@ -30,31 +30,31 @@ HTML(
 }
 
 ##
-tabPanel.msdplot <- function(plot.id){
+tabPanel.msdplot <- function(plot.id, width ="80%"){
 
 tabPanel("均值和方差图", 
 
 p(br()),
 
-plotly::plotlyOutput(plot.id)
+plotly::plotlyOutput(plot.id, width = width)
 
 )
 }
 
 
 ##
-tabPanel.pdfplot <- function(plot.id1, plot.id2, plot.id3, bin.id){
+tabPanel.pdfplot <- function(plot.id1, plot.id2, plot.id3, bin.id, width = 500){
 
 tabPanel("分布图", 
 
 p(br()),
 
 p(tags$b("正态QQ图")),
-plotly::plotlyOutput(plot.id1),
+plotly::plotlyOutput(plot.id1, width = width),
 p(br()),
 
 p(tags$b("直方图")),
-plotly::plotlyOutput(plot.id2),
+plotly::plotlyOutput(plot.id2, width = width),
 p(br()),
 
 sliderInput(bin.id,"直方图的分箱数",min = 0,max = 100,value = 0),
@@ -63,7 +63,7 @@ p(br()),
 
 
 p(tags$b("概率密度函数图")),
-plotly::plotlyOutput(plot.id3),
+plotly::plotlyOutput(plot.id3, width = width),
 p(br()),
 
 HTML(
@@ -83,27 +83,35 @@ HTML(
 
 ####==========================================================================================
 
-tabPanel.histplot <- function(plot.id1, plot.id2){
+## NPtest 
+
+tabPanel.histplot <- function(plot.id1, plot.id2, bin.id, width = 500){
 
 
 tabPanel("直方图", 
 p(br()),
 
+p(tags$b("直方图")),
+plotly::plotlyOutput(plot.id1, width = width),
+p(br()),
+
+sliderInput(bin.id, "直方图的分箱数",min = 0,max = 100,value = 0),
+p("当分箱数为0时，绘图将使用默认分箱数。"),
+p(br()),
+
+p(tags$b("密度图")),
+plotly::plotlyOutput(plot.id2, width = width),
+p(br()),
+
 HTML(
-"<b> 说明 </b>
+"
+<b> 说明 </b>
 <ul>
 <li> 直方图：通过描述某一数值范围内出现的观察值频率，粗略评估给定变量的概率分布
 <li> 密度图：估计数据的概率密度函数
 </ul>
 "
-),
-p(tags$b("直方图")),
-plotly::plotlyOutput("makeplot"),
-
-sliderInput("bin","直方图的分箱数",min = 0,max = 100,value = 0),
-p("当分箱数为0时，绘图将使用默认分箱数。"),
-p(tags$b("密度图")),
-plotly::plotlyOutput("makeplot.1")
+)
 
 )
 }
