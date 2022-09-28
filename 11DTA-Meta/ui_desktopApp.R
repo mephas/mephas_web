1c2c8234829f6fc867ed60f334bb5fcfcd9e3c55
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-09-28 17:15:21 +0900

    mo

--- 11DTA-Meta/ui_desktopApp.R
+++ 11DTA-Meta/ui_desktopApp.R
@@ -1,3 +1,7 @@
 mainPanel(
-  downloadButton("desktopApp_Download","Download App")
+  downloadButton("desktopApp_Download","Download App"),
+  HTML(
+    "<br><b><h4>This is Page to get DTA Meta Desktop App.You can get an EXE file from the above button.The EXE file is installer, run it after downloading.
+    <br>If you have any questions about how to install, <a href='https://alain003.phs.osaka-u.ac.jp/mephas/index.html' target='_brank'> please click here.</a></h4><b>"
+  )
 )
