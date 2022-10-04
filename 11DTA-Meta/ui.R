dd2f221f0ae8a33b6fb606bff636abeadb39fed0
yz-apr <zhouyi0426@hotmail.com>
2022-10-05 00:09:10 +0800

    update

--- 11DTA-Meta/ui.R
+++ 11DTA-Meta/ui.R
@@ -9,13 +9,15 @@ if (!requireNamespace("meta", quietly = TRUE)) {install.packages("meta")}; requi
 if (!requireNamespace("mada", quietly = TRUE)) {install.packages("mada")}; require("mada",quietly = TRUE)
 if (!requireNamespace("dtametasa", quietly = TRUE)) {install.packages("dtametasa")}; require("dtametasa",quietly = TRUE)
 if (!requireNamespace("stats", quietly = TRUE)) {install.packages("stats")}; require("stats",quietly = TRUE)
-library("shinyAce")
+if (!requireNamespace("shinyAce", quietly = TRUE)) {install.packages("shinyAce")}; require("shinyAce",quietly = TRUE)
+
+
 #library("bsplus")
 #library("htmltools")
 source("function.R")
 #source("./ui_dtameta.R",local = TRUE)
-source("./ui_uni.R",local = TRUE)
-source("../tab/tab.R")
+source("ui_uni.R",local = TRUE)
+source("ui_tab.R")
 source("../tab/panel.R")
 source("../tab/func.R")
 source("./sauc.R")
@@ -24,44 +26,46 @@ source("./dtametasa_fc.R")
 
 
 tagList(includeCSS("../www/style.css"),
-        stylink(),
-        
-        fluidPage(
-          shinyWidgets::switchInput(
-            inputId = "explain_on_off",#
-            label = "<i class=\"fa fa-book\"></i>", # Explanation in Details
-            inline = TRUE,
-            onLabel = "Close",
-            offLabel = "Information",
-            size = "mini"
-          )
-        )
-        ,
+stylink(),
 
-  #shinythemes::themeSelector(),
+fluidPage(
+shinyWidgets::switchInput(
+inputId = "explain_on_off",#
+label = "<i class=\"fa fa-book\"></i>", # Explanation in Details
+inline = TRUE,
+onLabel = "Close",
+offLabel = "Information",
+size = "mini"
+)
+)
+,
+
+#shinythemes::themeSelector(),
 navbarPage(
-  theme = shinythemes::shinytheme("flatly"),
-  collapsible = TRUE,
-  position="fixed-top",
-  title = "Meta-Analysis of Diagnostic Test Accuracy",
-  conditionalPanel(
-  	condition = "input.explain_on_off",
-  	HTML(
-  		"
-<b>Meta-analysis of diagnostic test accuracy (DTA meta) </b> is used to summaries results of diagnostic studies
+theme = shinythemes::shinytheme("flatly"),
+collapsible = TRUE,
+position="fixed-top",
+title = "Meta-Analysis of Diagnostic Test Accuracy",
+conditionalPanel(
+condition = "input.explain_on_off",
+HTML(
+"
+<b>Meta-analysis of diagnostic test accuracy (DTA meta) </b> is used to summaries results of multiple diagnostic studies
 
 <h4><b> 1. Functionalities  </b></h4>
 <ul>
-<li> To upload data files, preview data set, and check the correctness of data input</li>
+<li> To upload data files, preview data set, and check the correctness of data input
 <li> Two models are available: (1) bivariate random-effect model (Reitsma et al. 2005) or 
-(2) HSROC model (Rutter and Gatsonis, 2001)</li>
-<li> To produce summary ROC (SROC) curves and summary AUC (SAUC)</li>
+(2) HSROC model (Rutter and Gatsonis, 2001)
+<li> To produce summary ROC (SROC) curves and summary AUC (SAUC)
+<li> To detect the publication bias in the results
+<li> To do sensitivity analysis for the publication bias
 </ul>
 
-<h4><b> 2. About your data (training set) </b></h4>
+<h4><b> 2. The format of your data</b></h4>
 
 <ul>
-<li> The variables in your data should be names as <b>TP, FP, TN, FN </b>, indicating true positive, false positive, true negative, false negative, respectively.  </li>
+<li> The variables in your data must contains names as <b>TP, FP, TN, FN </b>, indicating true positive, false positive, true negative, false negative, respectively.  
 </ul>
 
 <h4><i>Case Example</i></h4>
@@ -70,29 +74,34 @@ navbarPage(
 
 </i>
 
-<h4> Please follow the <b>Steps</b>, and <b>Outputs</b> will give real-time analytical results. </h4>
+<h4> Please follow the <b>Steps</b>, and <b>Outputs</b> will give the analytical results. </h4>
 "
-  		
-  	),
-  	p("This page can make",tags$strong(" SROC Plot")," and",tags$strong(" SAUC Plot")),
-  	HTML('<b>&#9312;Set Header TP,FN,FP,TN</b>'),br(),
-  	HTML("&#10103;<b>When Change the Data, Click Reload DATA TO Calculation</b>"),br(),
-  	downloadButton("ManualPDF_download","Download Manual")
-  ),
-  tabPanel("Meta-Analysis",
-    headerPanel("Meta-Analysis of Diagnostic Studies"),
-    source("./ui_dta.R",local = TRUE)$value),
-  tabPanel("Funnel Plot",
-    headerPanel("Funnel Plot"),
-    source("./ui_funnel.R",local=TRUE)$value
-  ),
-  tabPanel("Sensitivity Analysis",headerPanel("Sensitivity-Analysis of Diagnostic Studies"),
-  	source("./ui_sensitivity.R",local = TRUE)$value
-  									),
-  tabPanel("DesktopApp",headerPanel("Download DTA Meta Desktop App"),
-    source("./ui_desktopApp.R",local = TRUE)$value),
-  #tablang("11DTA-Meta"),
-  tabstop(),
-  tablink(),
-  
-  ))
+
+),
+
+# p("This page can make",tags$strong(" SROC Plot")," and",tags$strong(" SAUC Plot")),
+# HTML('<b>&#9312;Set Header TP,FN,FP,TN</b>'),br(),
+# HTML("&#10103;<b>When Change the Data, Click Reload DATA TO Calculation</b>"),br(),
+downloadButton("ManualPDF_download","Download Manual")
+),
+tabPanel("Meta-Analysis",
+headerPanel("Meta-Analysis of Diagnostic Test Accuracy"),
+source("ui_dta.R",local = TRUE)$value),
+
+tabPanel("Funnel Plot",
+headerPanel("Funnel Plot of the Diagnistic Studies for Detecting Publication Bias"),
+source("ui_funnel.R",local=TRUE)$value
+),
+tabPanel("Sensitivity Analysis",
+  headerPanel("Sensitivity-Analysis of the Publicatin Bias"),
+source("ui_sensitivity.R",local = TRUE)$value
+),
+tabPanel("Download App",
+headerPanel("Download and Install the Desktop App"),
+source("ui_desktopApp.R",local = TRUE)$value),
+
+#tablang("11DTA-Meta"),
+tabstop(),
+tablink(),
+
+))
--- 11DTA-Meta/ui_desktopApp.R
+++ 11DTA-Meta/ui_desktopApp.R
@@ -1,7 +1,10 @@
 mainPanel(
   downloadButton("desktopApp_Download","Download App"),
   HTML(
-    "<br><b><h4>This is Page to get DTA Meta Desktop App.You can get an EXE file from the above button.The EXE file is installer, run it after downloading.
+    "<br><b><h4>You can get an EXE installation file by clicking the above button.
+
+    The EXE file is onlu applicable for Windows system.
+    
     <br>If you have any questions about how to install, <a href='https://alain003.phs.osaka-u.ac.jp/mephas/index.html' target='_brank'> please click here.</a></h4><b>"
   )
 )
