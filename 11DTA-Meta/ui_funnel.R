a2fb03a66c64ec2cbb90790edd912cbdffbe925d
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-10-05 11:49:22 +0900

    v

--- 11DTA-Meta/ui_funnel.R
+++ 11DTA-Meta/ui_funnel.R
@@ -8,7 +8,6 @@ sidebarLayout(
             tabPanel("Funnel plot",
                 p(plotOutput("ml.lnDOR_funnel",width=500),downloadButton("mo"),downloadButton("momof")),plotOutput("logit.Sens_plot",width=500),plotOutput("logit.Spec_plot",width=500)
 
-
                 )
         )
     )
