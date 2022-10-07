c11b25e05852c0ee1b0ce057f44cd97302c7ea2c
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2022-10-07 17:36:47 +0900

    moc

--- 11DTA-Meta/ui_desktopApp.R
+++ 11DTA-Meta/ui_desktopApp.R
@@ -3,7 +3,7 @@ mainPanel(
   HTML(
     "<br><b><h4>You can get an EXE installation file by clicking the above button.
 
-    The EXE file is onlu applicable for Windows system.
+    The EXE file is only applicable for Windows system.
     
     <br>If you have any questions about how to install, <a href='https://alain003.phs.osaka-u.ac.jp/mephas/index.html' target='_brank'> please click here.</a></h4><b>"
   )
