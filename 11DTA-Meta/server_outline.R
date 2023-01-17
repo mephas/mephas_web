d7dfc0d2fca2a573eaf3746b98cac1cc0fbb6d56
mizutani-i19 <ayame.i19@gmail.com>
2023-01-17 20:36:02 +0900

    escape

--- 11DTA-Meta/server_outline.R
+++
@@ -1,9 +0,0 @@
-output$ManualPDF_download<-downloadHandler("DTAmanual.pdf"
-                                             ,content = function(file){
-                                               file.copy("./DTAmanual.pdf",file)
-                                             }
-                                             ,contentType = "application/pdf")
-output$desktopApp_Download<-downloadHandler("Setup.exe",
-                                            content = function(file){
-                                              file.copy("./Dtametasa-setup-0.1.0.exe",file)}
-                                              )
--- 11DTA-Meta/ui_desktopApp.R
+++
@@ -1,9 +0,0 @@
-mainPanel(
-  downloadButton("desktopApp_Download","Download App"),
-  HTML(
-    "<p><h4>You can get an EXE installation file</p>.
-    <br></br>
-    The EXE file is currently only applicable for Windows system.
-    "
-  )
-)
