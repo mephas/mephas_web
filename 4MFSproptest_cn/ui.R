8aa12eece394bf48d0928f42dd3985beca2020dc
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2024-06-14 07:57:03 +0900

    Update ui.R

--- 4MFSproptest_cn/ui.R
+++ 4MFSproptest_cn/ui.R
@@ -21,7 +21,7 @@ tabOF(),
 ##########--------------------##########--------------------##########
 
 navbarPage(
-theme = shinythemes::shinytheme("cerulean"),
+#theme = shinythemes::shinytheme("cerulean"),
 #title = a("Test for Binomial Proportions", href = "https://alain003.phs.osaka-u.ac.jp/mephas/", style = "color:white;"),
 title = "二项式检验",
 collapsible = TRUE,
