f366ab81f53bfd50194edc36e7b1ed2cd6e5eb04
mizutani-i19 <39848859+mizutani-i19@users.noreply.github.com>
2024-06-24 20:01:23 +0900

    Update server_1.R

--- 6MFSanova_jp/server_1.R
+++ 6MFSanova_jp/server_1.R
@@ -8,7 +8,7 @@ names1 <- reactive({
 Y1.0 <- reactive({
   inFile <- input$file1
   match_sep<- input$seprate_data1 %in% c("カンマ", "行替え", "タブ","スペース","コロン","セミコロン")
-  	separater<-paste0(c(",","\n","\t"," ",":",";")[match_sep])
+  	separater<-paste0(c(",","\n","\t"," ",":",";")[match_sep],collapse="")
   if (is.null(inFile)) {
     X <- as.numeric(unlist(strsplit(input$x1, "[,;\n\t ]")))
     validate( need(sum(!is.na(X))>1, "Please input enough valid numeric data") )
